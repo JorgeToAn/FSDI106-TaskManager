a3ab611e2035f9da46d75345e261338f0b409bfa
JorgeToAn <jorgetosta.f@gmail.com>
2022-07-27 20:47:16 -0700

    Class 4 work

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+const GET_SERVER = "https://fsdiapi.azurewebsites.net/api/tasks";
+const POST_SERVER = "https://fsdiapi.azurewebsites.net/api/tasks/";
 const IMP_CLASS = "fa-solid";
 const NONIMP_CLASS = "fa-regular";
 let isImportant = false;
@@ -33,7 +35,6 @@ function hideForm() {
 }
 
 function addTask(){
-  console.log("Task button pressed!");
   let title = $("#txtTitle").val();
   let description = $("#txtDesc").val();
   let date = $("#dateDue").val();
@@ -45,31 +46,74 @@ function addTask(){
 
   let newTask = new Task(isImportant, title, description, date, location, color, emoji, status, notification);
   if(newTask.isValid()){
-    displayTask(newTask);
-    clearForm();
+    //send task to the server
+    $.ajax({
+      type: "POST",
+      url: POST_SERVER,
+      data: JSON.stringify(newTask),
+      contentType: "application/json",
+      success: function(response) {
+        console.log("Server says", response);
+        displayTask(newTask);
+        clearForm();
+      },
+      error: function(errorDetails) {
+        console.log("Error saving task", errorDetails);
+      }
+    });
+
   }
 }
 
-function displayTask(task) {
-  console.log("Displaying", task);
-  let syntax = 
-  `<div class="task-block">
-    <div class="task-title">
-      <h4>${task.title}</h4>
-      <hr class="x-solid">
-      <p>${task.description}</p>
-    </div>
-    <div class="task-info">
-      <p><i class="fa-solid fa-clock"></i> ${task.date}</p>
-      <p><i class="fa-solid fa-location-dot"></i> ${task.location}</p>
-    </div>
-    <div class="task-status">
-      <p>${task.status}</p>
-    </div>
-    <div class="task-remove">
-      <i class="fa-solid fa-xmark"></i>
-    </div>
+function createSyntax(task) {
+  let syntax = `<div class="task-block" style="border: 2px solid ${task.color}">`;
+
+  if(task.important){
+    syntax += 
+    `<div class="task-importance">
+      <i class="${IMP_CLASS} fa-star"></i>
+      <p>${task.emoji}</p>
+    </div>`;
+  } else {
+    syntax += 
+    `<div class="task-importance">
+      <i class="${NONIMP_CLASS} fa-star"></i>
+      <p>${task.emoji}</p>
+    </div>`;
+  }
+  syntax += 
+  `<div class="task-title">
+    <h4>${task.title}</h4>
+    <hr class="x-solid">
+    <p>${task.description}</p>
+  </div>
+  <div class="task-info">
+    <p><i class="fa-solid fa-clock"></i> ${task.date}</p>
+    <p><i class="fa-solid fa-location-dot"></i> ${task.location}</p>
   </div>`;
+  if(task.notification){
+    syntax += 
+    `<div class="task-status">
+      <p>${task.status}</p>
+      <p>Will notify</p>
+    </div>`;
+  } else {
+    syntax += 
+    `<div class="task-status">
+      <p>${task.status}</p>
+    </div>`;
+  }
+  syntax += 
+  `<div class="task-remove">
+    <i class="fa-solid fa-xmark"></i>
+  </div>
+  </div>`;
+
+  return syntax;
+}
+
+function displayTask(task) {
+  let syntax = createSyntax(task);
 
   $(document).on("click", "i.fa-xmark", function(){
     $(this).parent().parent().remove();
@@ -88,12 +132,34 @@ function clearForm() {
   $("#selStatus").val("New");
 }
 
-function init() {
-  console.log("Starting init...");
+function fetchTasks() {
+  $.ajax({
+    type: "GET",
+    url: GET_SERVER,
+    success: function(response) {
+      let tasks = JSON.parse(response);
+      for(let i=0; i<tasks.length; i++) {
+        let task = tasks[i];
+        if(task.name == "Jorge"){
+          displayTask(task);
+        }
+      }
 
+    },
+    error: function(errorDetails) {
+      console.log("Error fetching tasks", errorDetails);
+    }
+  })
+}
+
+function init() {
+  //hook events
   $("#icoImportant").click(toggleImportant);
   $("#btnHide").click(hideForm);
   $("#btnAddTask").click(addTask);
+
+  //load initial data
+  fetchTasks();
 }
 
 window.onload = init;
